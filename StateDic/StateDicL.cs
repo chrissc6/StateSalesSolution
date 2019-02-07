406b6533d4874c747c82584f67328f48d4cec2b3
Chris Caudill <christopherscaudill@gmail.com>
2019-02-07 16:37:29 -0500

    init-c

---
+++ StateDic/StateDicL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDic
+{
+    public class StateDicL
+    {
+        private Dictionary<string, int> ststr = new Dictionary<string, int>();
+
+        public void Add(string key, int data)
+        {
+            ststr.Add(key, data);
+        }
+
+        public int Get(string key)
+        {
+            return ststr[key];
+        }
+
+        public List<int> Vaules()
+        {
+            return ststr.Values.ToList();
+        }
+
+    }
+}
--- StateSales/Program.cs
+++ StateSales/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StateDic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace StateSales
     {
         static void Main(string[] args)
         {
+            var strDicton = new StateDicL();
+            strDicton.Add("OH", 90416);
+            strDicton.Add("KY", 77408);
+            strDicton.Add("IN", 16588);
+            strDicton.Add("PA", 55043);
+            strDicton.Add("MI", 60099);
+            strDicton.Add("NY", 19830);
+            strDicton.Add("IL", 81154);
+            strDicton.Add("WI", 11679);
+            strDicton.Add("MN", 94207);
+            strDicton.Add("MS", 93521);
+
+            var total = 0;
+
+            total += strDicton.Get("IN");
+            total += strDicton.Get("NY");
+            total += strDicton.Get("MN");
+            Console.WriteLine($"Total Sales of IN, NY, MN: {total}");
+
+            total = 0;
+
+            total += strDicton.Get("KY");
+            total += strDicton.Get("PA");
+            total += strDicton.Get("MI");
+            total += strDicton.Get("IL");
+            Console.WriteLine($"Total Sales of KY, PA, MI, IL: {total}");
+
         }
     }
 }
